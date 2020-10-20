c1d35eeec33b8f7756d33f66b0fd53408c34a1bf
majiexiong <1455080341@qq.com>
2020-10-20 17:50:41 +0800

    test

---
+++ test_case/test_requests_1.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class Test_requests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+    def setUp(self) -> None:
+        pass
+    def tearDown(self) -> None:
+        pass
+    def test_00(self):
+        pass
